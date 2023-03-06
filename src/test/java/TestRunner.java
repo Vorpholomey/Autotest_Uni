754399e0ebb9f5ecad5cbf12cbc295e1a586629a
Vorpholomey <117433309+Vorpholomey@users.noreply.github.com>
2023-03-06 15:31:12 +0700

    Доработка тест раннера

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "scr/test/java/Steps/glue"
+        glue = "/Steps/glue"
 
 )
 public class TestRunner {
